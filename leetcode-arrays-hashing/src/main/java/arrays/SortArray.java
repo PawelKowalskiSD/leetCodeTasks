3ac0005d29f0bde80f32452cedf03b1fd1be4d29
Pawel Kowalski <kowalski.pawel.sd@gmail.com>
2024-01-09 23:38:11 +0100

    Sort Array

--- leetcode-arrays-hashing/src/main/java/arrays/SortArray.java
+++ leetcode-arrays-hashing/src/main/java/arrays/SortArray.java
@@ -2,7 +2,56 @@ package arrays;
 
 public class SortArray {
     public int[] sort(int[] nums) {
-
-        return nums;
+        return mergeSort(nums, 0, nums.length - 1);
     }
+
+    private int[] mergeSort(int[] array, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort(array, left, mid);
+            mergeSort(array, mid + 1, right);
+            merge(array, left, mid, right);
+        }
+        return array;
+    }
+
+    private void merge(int[] array, int left, int mid, int right) {
+        int leftSize = mid - left + 1;
+        int rightSize = right - mid;
+
+        int[] leftArray = new int[leftSize];
+        int[] rightArray = new int[rightSize];
+
+        for (int i = 0; i < leftSize; i++) {
+            leftArray[i] = array[left + i];
+        }
+        for (int j = 0; j < rightSize; j++) {
+            rightArray[j] = array[mid + 1 + j];
+        }
+
+        int i = 0, j = 0, k = left;
+        while (i < leftSize && j < rightSize) {
+            if (leftArray[i] <= rightArray[j]) {
+                array[k] = leftArray[i];
+                i++;
+            } else {
+                array[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < leftSize) {
+            array[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        while (j < rightSize) {
+            array[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+
 }
