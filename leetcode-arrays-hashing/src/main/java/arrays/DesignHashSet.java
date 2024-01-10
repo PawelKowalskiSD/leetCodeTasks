30a266b4a93990b35403f92c10911145339759ce
Pawel Kowalski <kowalski.pawel.sd@gmail.com>
2024-01-10 22:49:08 +0100

    Design HashSet

---
+++ leetcode-arrays-hashing/src/main/java/arrays/DesignHashSet.java
@@ -0,0 +1,26 @@
+package arrays;
+
+public class DesignHashSet {
+    private int key;
+    private int[] arrays = new int[9];
+
+
+    public DesignHashSet() {
+
+    }
+
+    public void add(int key) {
+
+    }
+
+    public void remove(int key) {
+
+    }
+
+    public boolean contains(int key) {
+        if(this.key == key)
+            return true;
+        return false;
+
+    }
+}
