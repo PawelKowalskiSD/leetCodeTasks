2af8a292b393bfdd0b763f277dab25edf99f2f53
Pawel Kowalski <kowalski.pawel.sd@gmail.com>
2024-01-13 23:03:19 +0100

    Product of Array Except Self

--- leetcode-arrays-hashing/src/main/java/arrays/ProductOfArrayExceptSelf.java
+++ leetcode-arrays-hashing/src/main/java/arrays/ProductOfArrayExceptSelf.java
@@ -3,6 +3,17 @@ package arrays;
 public class ProductOfArrayExceptSelf {
 
     public int[] productExceptSelf(int[] nums) {
-        return nums;
+        int[] answer = new int[nums.length];
+        int sum = 1;
+        for (int i = 0; i < nums.length; i++) {
+            answer[i] = sum;
+            sum *= nums[i];
+        }
+        int result = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            answer[i] *= result;
+            result *= nums[i];
+        }
+        return answer;
     }
 }
