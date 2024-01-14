df1bb1036d00582b860f3875f168f230d097a098
Pawel Kowalski <kowalski.pawel.sd@gmail.com>
2024-01-14 23:14:36 +0100

    Valid Sudoku

---
+++ leetcode-arrays-hashing/src/main/java/arrays/ValidSudoku.java
@@ -0,0 +1,11 @@
+package arrays;
+
+public class ValidSudoku {
+    public boolean isValidSudoku(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+            }
+        }
+        return true;
+    }
+}
