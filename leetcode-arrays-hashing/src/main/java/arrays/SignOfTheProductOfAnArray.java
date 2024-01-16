0177d6de7fd32bef37fb8ec90c3a9cd19b250675
Pawel Kowalski <kowalski.pawel.sd@gmail.com>
2024-01-16 11:40:43 +0100

    Sing of the product of an Array

--- leetcode-arrays-hashing/src/main/java/arrays/SignOfTheProductOfAnArray.java
+++ leetcode-arrays-hashing/src/main/java/arrays/SignOfTheProductOfAnArray.java
@@ -1,17 +1,26 @@
 package arrays;
 
+import java.math.BigDecimal;
+
 public class SignOfTheProductOfAnArray {
     public int arraySign(int[] nums) {
         int output = 0;
         int multiply = 1;
         for(int i = 0; i < nums.length; i++) {
             multiply *= nums[i];
+
+            if (multiply > 0) {
+                multiply = 1;
+            } else if (multiply < 0) {
+                multiply = -1;
+            }
         }
-        if(multiply > 0) {
-            output = 1;
-        } else if(multiply < 0) {
-            output = -1;
-        }
+            if (multiply > 0) {
+                output = 1;
+            } else if (multiply < 0) {
+                output = -1;
+            }
+
         return output;
     }
 }
--- leetcode-arrays-hashing/src/test/java/arrays/SignOfTheProductOfAnArrayTest.java
+++ leetcode-arrays-hashing/src/test/java/arrays/SignOfTheProductOfAnArrayTest.java
@@ -13,13 +13,19 @@ class SignOfTheProductOfAnArrayTest {
         int[] input1 = {-1,-2,-3,-4,3,2,1};
         int[] input2 = {1,5,0,2,-3};
         int[] input3 = {-1,1,-1,1,-1};
+        int[] input4 = {41,65,14,80,20,10,55,58,24,56,28,86,96,10,3,84,4,41,13,32,42,43,83,78,82,70,15,-41};
+        int[] input5 = {9,72,34,29,-49,-22,-77,-17,-66,-75,-44,-30,-24};
         //When
         int result1 = signOfTheProductOfAnArray.arraySign(input1);
         int result2 = signOfTheProductOfAnArray.arraySign(input2);
         int result3 = signOfTheProductOfAnArray.arraySign(input3);
+        int result4 = signOfTheProductOfAnArray.arraySign(input4);
+        int result5 = signOfTheProductOfAnArray.arraySign(input5);
         //Then
         assertEquals(1, result1);
         assertEquals(0, result2);
         assertEquals(-1, result3);
+        assertEquals(-1, result4);
+        assertEquals(-1, result5);
     }
 }
