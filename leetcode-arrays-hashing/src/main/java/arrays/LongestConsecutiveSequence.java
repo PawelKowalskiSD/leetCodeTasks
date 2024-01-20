f793e587986cb19b671c16f829427e4685edc385
Pawel Kowalski <kowalski.pawel.sd@gmail.com>
2024-01-20 23:30:02 +0100

    Longest Consecutive Sequence

--- leetcode-arrays-hashing/src/main/java/arrays/LongestConsecutiveSequence.java
+++ leetcode-arrays-hashing/src/main/java/arrays/LongestConsecutiveSequence.java
@@ -1,13 +1,25 @@
 package arrays;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class LongestConsecutiveSequence {
     public int longestConsecutive(int[] nums) {
-        Arrays.sort(nums);
-        for (int i =0; i < nums.length; i++){
-
+        Set<Integer> result = new HashSet<>();
+        int count = 0;
+        for(Integer sets : nums) {
+            result.add(sets);
         }
-        return 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(!result.contains(nums[i] - 1)) {
+                int size = 0;
+                while(result.contains(nums[i] + size)) {
+                    size += 1;
+                    count = Math.max(size, count);
+                }
+            }
+        }
+        return count;
     }
 }
