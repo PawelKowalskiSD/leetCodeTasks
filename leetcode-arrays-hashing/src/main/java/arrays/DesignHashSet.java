8e1d0b06b6685f32a0308d655600710bfb64e731
Pawel Kowalski <kowalski.pawel.sd@gmail.com>
2024-01-22 20:30:26 +0100

    Design HashSet

--- leetcode-arrays-hashing/src/main/java/arrays/DesignHashSet.java
+++ leetcode-arrays-hashing/src/main/java/arrays/DesignHashSet.java
@@ -1,26 +1,21 @@
 package arrays;
 
 public class DesignHashSet {
-    private int key;
-    private int[] arrays = new int[9];
-
+    boolean[] hashSet;
 
     public DesignHashSet() {
-
+        hashSet = new boolean[(int) 1e6 + 1];
     }
 
     public void add(int key) {
-
+        hashSet[key] = true;
     }
 
     public void remove(int key) {
-
+        hashSet[key] = false;
     }
 
     public boolean contains(int key) {
-        if(this.key == key)
-            return true;
-        return false;
-
+        return hashSet[key];
     }
 }
