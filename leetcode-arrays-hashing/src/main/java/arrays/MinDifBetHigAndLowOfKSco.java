d629eefb35ce91b3c9a1e51d51e0dd205165adde
Pawel Kowalski <kowalski.pawel.sd@gmail.com>
2024-01-26 20:00:51 +0100

    Minimum Difference Between Highest and Lowest of K Scores

---
+++ leetcode-arrays-hashing/src/main/java/arrays/MinDifBetHigAndLowOfKSco.java
@@ -0,0 +1,19 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class MinDifBetHigAndLowOfKSco {
+    public int minimumDifference(int[] nums, int k) {
+        if(k == 1) {
+            return 0;
+        }
+        Arrays.sort(nums);
+        int result = Integer.MAX_VALUE;
+        for(int i = 0; i < nums.length - k + 1; i++) {
+            int diff = Math.abs(nums[i] - nums[i + k - 1]);
+            System.out.println(diff);
+            result = Math.min(diff, result);
+        }
+        return result;
+    }
+}
---
+++ leetcode-arrays-hashing/src/test/java/arrays/MinDifBetHigAndLowOfKScoTest.java
@@ -0,0 +1,26 @@
+package arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MinDifBetHigAndLowOfKScoTest {
+
+    @Test
+    void shouldGetMinDiff() {
+        //given
+        MinDifBetHigAndLowOfKSco minDifBetHigAndLowOfKSco = new MinDifBetHigAndLowOfKSco();
+        int[] input1 = {90};
+        int[] input2 = {9, 4, 1, 7};
+        int[] input3 = {87063,61094,44530,21297,95857,93551,9918};
+        //When
+//        int result1 = minDifBetHigAndLowOfKSco.minimumDifference(input1, 1);
+//        int result2 = minDifBetHigAndLowOfKSco.minimumDifference(input2, 2);
+        int result3 = minDifBetHigAndLowOfKSco.minimumDifference(input3, 6);
+        //Then
+//        assertEquals(0, result1);
+//        assertEquals(2, result2);
+        assertEquals(74560, result3);
+    }
+
+}
