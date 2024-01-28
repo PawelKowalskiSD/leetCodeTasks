a6c9206e1bdfab603ae7494268f4a87a64a99848
Pawel Kowalski <kowalski.pawel.sd@gmail.com>
2024-01-28 20:55:09 +0100

    Merge Strings Alternately

--- leetcode-arrays-hashing/src/main/java/arrays/MergeStringsAlternately.java
+++ leetcode-arrays-hashing/src/main/java/arrays/MergeStringsAlternately.java
@@ -2,6 +2,21 @@ package arrays;
 
 public class MergeStringsAlternately {
     public String mergeAlternately(String word1, String word2) {
-        return null;
+        String result = "";
+        String end = "";
+        int size = word1.length();
+        if(word1.length() > word2.length()) {
+            size = word2.length();
+            end = word1.substring(word2.length(), word1.length());
+        } else if(word1.length() < word2.length()) {
+            size = word1.length();
+            end = word2.substring(word1.length(), word2.length());
+        }
+        for(int i = 0; i < size; i++) {
+            result += word1.charAt(i);
+            result += word2.charAt(i);
+        }
+        System.out.println(result);
+        return result + end;
     }
 }
